8f48b5a696737d6ad5ace83ebb419817e7bf88b4
Qianhui <helenyu301@gmail.com>
2025-01-15 17:01:32 -0500

    be able to read from input and show successfully

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,25 @@ class PayCalculatorApp extends StatefulWidget{
 // create a state here
 class _PayState extends State<PayCalculatorApp> {
   // here to declare the variables and functions, before the override
-  
+  // we should create a seperate TextEditingController for different textfield
+  var hoursController = TextEditingController();// controller for work hours
+  var rateController = TextEditingController(); // controller for hourly rate
+  var readHourInput = "";
+  // declare the showMessage function
+  void showResult(){
+    setState(() {
+      readHourInput = hoursController.text;
+    });
+  }
+
+  // we need to clean it up otherwise the memory will leak
+  @override
+  void dispose() {
+    hoursController.dispose();
+    rateController.dispose();
+    super.dispose();
+  }
+
   // since it inherites from State, we need to override
   @override
   Widget build(BuildContext context) {
@@ -39,23 +57,19 @@ class _PayState extends State<PayCalculatorApp> {
                 children: [
                   Text('Number of hours:', 
                   style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),),
-                  const SizedBox(
+                  SizedBox(
                     width: 150,
+                    // link controller here
                     child: TextField(
-                      decoration: InputDecoration(
+                      controller: hoursController,
+                      decoration: const InputDecoration(
                         border: OutlineInputBorder(),
-                        hintText: '123...'
-                        
-                      ),
-                    ),
-                  )
-                  
-                ],
-
-              )
-
+                        hintText: '123...' ),),
+                  )],
+              ),
+              Text(readHourInput),
+              ElevatedButton(onPressed: showResult, child: const Text("Calculate"))
             ],
-
           ),
         ),
       ),
