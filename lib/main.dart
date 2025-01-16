f7cbbbe1dbffac43f3054d60cd1d8e4bd11b538b
Qianhui <helenyu301@gmail.com>
2025-01-16 13:46:51 -0500

    basic math and app design finished, outputs are correct, need to improve the name information of the bottom

--- lib/main.dart
+++ lib/main.dart
@@ -16,12 +16,34 @@ class _PayState extends State<PayCalculatorApp> {
   // we should create a seperate TextEditingController for different textfield
   var hoursController = TextEditingController();// controller for work hours
   var rateController = TextEditingController(); // controller for hourly rate
-  var readHourInput = "";
-  // declare the showMessage function
+  var readHourInput = 0.0;
+  var readRateInput = 0.0;
+  var regular = 0.0;
+  var overTime = 0.0;
+  var totalPayBeforeT = 0.0;
+  var tax = 0.0;
+  // declare the showMessage function, later this can show the result and also calculate
   void showResult(){
     setState(() {
-      readHourInput = hoursController.text;
-      // next: add readRateInput here
+      // assign the value to the variables from the input
+      readHourInput = double.parse(hoursController.text);
+      readRateInput = double.parse(rateController.text);
+      /* do calculation:
+      > 40 hrs, (inout - 40) * 1.5 * rate = overtime pay
+      <= 40hrs, input * rate
+      tax: pay * 0.8  
+      */
+      if (readHourInput > 40) {
+        overTime = (readHourInput - 40) * readRateInput * 1.5; // overtime pay
+        regular = 40 * readRateInput; // regular pay
+      }
+      else {
+        overTime = 0.0;
+        regular = readHourInput * readRateInput;
+      }
+      totalPayBeforeT = regular + overTime;
+      tax = totalPayBeforeT * 0.18;
+      
     });
   }
 
@@ -32,14 +54,13 @@ class _PayState extends State<PayCalculatorApp> {
     rateController.dispose();
     super.dispose();
   }
-
   // since it inherites from State, we need to override
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         // the following two are stateless widget
-        backgroundColor: const Color.fromARGB(255, 225, 234, 221),
+        backgroundColor: const Color.fromARGB(255, 228, 234, 239),
         appBar: AppBar(
           backgroundColor: Color.fromRGBO(253, 252, 252, 0),
           title: Text('Pay Calculator', 
@@ -54,22 +75,61 @@ class _PayState extends State<PayCalculatorApp> {
               SizedBox(height: 50,),
               // one input field combination
               Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
                 children: [
-                  Text('Number of hours:', 
+                  Text('Number of hours: ', 
                   style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),),
                   SizedBox(
-                    width: 150,
-                    // link controller here
+                    width: 170,
                     child: TextField(
+                      keyboardType: TextInputType.numberWithOptions(
+                        decimal: true
+                      ),
+                      // link controller
                       controller: hoursController,
                       decoration: const InputDecoration(
                         border: OutlineInputBorder(),
                         hintText: '123...' ),),
                   )],
               ),
-              Text(readHourInput),
-              ElevatedButton(onPressed: showResult, child: const Text("Calculate"))
+              SizedBox(height: 30,),
+              // another row for the rate input
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text("Hourly Rate:     ",
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.w500),),
+                  SizedBox(
+                    width: 200,
+                    child: TextField(
+                      keyboardType: TextInputType.numberWithOptions(
+                        decimal: true
+                      ),
+                      controller: rateController,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: '123...'
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              // adding other elements here as a child of cloumn
+              // Text(readRateInput), // for debugging only
+              ElevatedButton(onPressed: showResult, 
+              child: const Text("Calculate", style: TextStyle(fontSize: 23),)),
+              Text(regular.toStringAsFixed(2)), // for debugging only
+              Text(overTime.toStringAsFixed(2)), // for debugging only
+              Text(totalPayBeforeT.toStringAsFixed(2)), // for debugging only
+              Text(tax.toStringAsFixed(2)), // for debugging only
+              // adding other elements here as a child of cloumn
+              Row(
+                
+                children: [
+                  Text("Qianhui Yu"),
+                  Text("301462989")
+                ],
+              )
             ],
           ),
         ),
